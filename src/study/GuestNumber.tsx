5487f9ba7e564aeecd2ea961e20e9f7b7c1d670b
李佐群 <2645929971@qq.com>
2024-12-26 15:24:26 +0800

    feat(study): 添加猜数字小游戏
    - 新增 GuestNumber 组件，实现了一个简单的猜数字游戏- 在 AppInit 中引入并使用 GuestNumber 组件
    - 更新 README.md，添加项目描述和结构说明
    - 修改 .gitignore，添加 node_modules 到忽略列表
    - 删除无用的 1.txt 文件

---
+++ src/study/GuestNumber.tsx
@@ -0,0 +1,53 @@
+import {Alert, Button, SafeAreaView, StyleSheet, TextInput} from 'react-native';
+import React from 'react';
+
+let number: number;
+let count: number = 0;
+const GuestNumber = () => {
+
+  const [text, changeText] = React.useState('');
+
+  // 组件渲染完成后，初始化Number
+  React.useEffect(() => {
+    initNumber();
+  }, []);
+
+  const initNumber = () => {
+    number = Math.round(Math.random() * 100);
+    count = 0;
+    console.log('初始化Number--', number);
+  };
+
+  const doGuest = () => {
+    count++;
+    if (number > Number(text)) {
+      Alert.alert('你输入的数字太小了');
+    } else if (number < Number(text)) {
+      Alert.alert('你输入的数字太大了');
+    } else {
+      Alert.alert(`猜对了，这个数就是${number}, 你总共猜了${count}次`);
+    }
+  };
+
+  return <SafeAreaView>
+    <TextInput style={styles.input} value={text} onChangeText={changeText}/>
+    <Button
+      onPress={doGuest}
+      title="Please Guest"
+      color="#841584"
+    />
+  </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  input: {
+    textAlign: 'center',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 8,
+    margin: 10
+  }
+});
+
+export default GuestNumber;
